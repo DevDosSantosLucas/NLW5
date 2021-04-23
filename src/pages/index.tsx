3acdb1bb848af28f98e92c4ae2e79a27d00f85a3
DevDosSantosLucas <lucas1995ls@outlook.com>
2021-04-22 22:03:27 -0300

    Aula 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 //SSG
 import {GetStaticProps} from 'next';
 import Image from 'next/image';
+import Link from 'next/link'
 import {format,parseISO} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
 import { api } from '../services/api';
-import { cconvertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 import styles from './home.module.scss';
 
@@ -17,7 +18,6 @@ type Episode ={
   publishedAt: string;
   duration:number;
   durationAsString: string;
-  description: string;
   url: string
 }
 
@@ -41,8 +41,10 @@ export default function Home( { latestEpisodes,allEpisodes } : HomeProps ){
                        src= {episode.thumbnail} alt = {episode.title} 
                        objectFit = "cover"/>
                 
-                <div className = {styles.episodeDetails}>
-                  <a href = "">{episode.title}</a>
+                <div className = {styles.episodeDetail}>
+                  <Link href = {`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                    </Link>
                   <p>{episode.members}</p>
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
@@ -58,7 +60,51 @@ export default function Home( { latestEpisodes,allEpisodes } : HomeProps ){
       </section >
 
       <section className = {styles.allEpisodes}>
+          <h2> Todos episódios</h2>
 
+          <table cellSpacing={0}>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Podcast</th>
+                <th>Integrantes</th>
+                <th>Data</th>
+                <th>Duração</th>
+                <th></th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {allEpisodes.map(episode=>{
+                return(
+                  <tr key={episode.id}>
+                    <td style={{width: 80}}>
+                      <Image  
+                       width={120}
+                       height={120}
+                       src= {episode.thumbnail}
+                       alt = {episode.title}
+                       objectFit = "cover"
+                       /> 
+                    </td>
+                    <td>
+                    <Link href = {`/episodes/${episode.id}`} >
+                      <a>{episode.title}</a>
+                    </Link>
+                    </td>
+                    <td>{episode.members}</td>
+                    <td style={{width: 100}}>{episode.publishedAt}</td>
+                    <td>{episode.durationAsString}</td>
+                    <td>
+                      <button type = "button">
+                        <img src="/play-green.svg" alt="Tocar episódio" />
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
       </section >
     </div>
   )
@@ -82,8 +128,7 @@ const episodes = data.map(episode => {
     members: episode.members,
     publishedAt: format(parseISO(episode.published_at),'d MMM yy',{locale: ptBR}),
     duration: Number(episode.file.duration),
-    durationAsString: cconvertDurationToTimeString(Number(episode.file.duration)),
-    description: episode.description,
+    durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
     url: episode.file.url,
   };
 })
